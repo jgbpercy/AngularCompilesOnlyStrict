57ebfccdc61af81f6ac4d4ff18ae2d48b41b8953
joe.percy <joe.percy@capp.co>
2020-02-24 16:05:26 +0000

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+interface Thinger {
+  innerObs: Observable<Wotsit>;
+}
+
+interface Wotsit {
+  doDar: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'type-check-issue';
+  thingers: Observable<Thinger[]> = of([
+    {
+      innerObs: of({ doDar: 'do-dar' }),
+    },
+    {
+      innerObs: of({ doDar: 'hello sailor' }),
+    },
+  ]);
 }
